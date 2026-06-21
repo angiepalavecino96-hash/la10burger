bf0612446c3ed016caf6c9ad41c20be0ba055400
angiepalavecino96-hash <angiepalavecino.96@gmail.com>
2026-06-21 13:54:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,6 @@
 // CONFIGURACIÓN - COMPLETAR ANTES DE PUBLICAR
 // ====================================================================
 
-// 1. Entrá a tu proyecto en supabase.com > Settings > API
-// 2. Copiá la "Project URL" y pegala abajo
-"https://ixgcgrgrsdordefaselb.supabase.co"
-// 3. Copiá la "anon public" key (NO la service_role) y pegala abajo
-//    Esta clave es segura de usar en el navegador, en el menú público.
-"sb_publishable_1zSM9Wi5pB-gO8P7QStAlQ_W9f21DEP"
+const SUPABASE_URL = "https://ixqcgrgrsdordefaselb.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_1zSM9Wi5pB-gO8P7QStAlQ_W9f21DEP";
