950871263efd98e72c8189736668fad1d736e7a5
angiepalavecino96-hash <angiepalavecino.96@gmail.com>
2026-06-21 14:02:03 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // CONFIGURACIÓN - COMPLETAR ANTES DE PUBLICAR
 // ====================================================================
 
-const SUPABASE_URL = "https://ixqcgrgrsdordefaselb.supabase.co";
+const SUPABASE_URL = "https://ixgcgrgrsdordefaselb.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_1zSM9Wi5pB-gO8P7QStAlQ_W9f21DEP";
